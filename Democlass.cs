1e8ca556130f4f49814d20f53c329f8b88b82e65
sleem3232@gmail.com <sleem3232@gmail.com>
2021-11-23 19:00:29 +0200

    Add project files.

---
+++ Democlass.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Exception_handling_and_Error_handling
+{
+  public  class Democlass
+    {
+        public int grandparent(int position)
+        {
+            int output = 0;
+            Console.WriteLine("open data base connection");
+            try
+            {
+                output = parentmethod(position);
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }            Console.WriteLine("close data base connection");
+            return output;
+
+        }
+        public int parentmethod(int position)
+        {
+            return Getnumber(position);
+        }
+        public int Getnumber(int position)
+        {
+            int output = 0;
+            try
+            {
+                int[] numbers = new int[] { 1, 4, 7, 2 };
+                output= numbers[position];
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);//Index was outside the bounds of the array.
+
+                Console.WriteLine(ex.StackTrace);//  at Exception_handling_and_Error_handling.
+                                                 //  Democlass.Getnumber(Int32 position) in
+                                                 //  C:\Users\PC\source\repos\Exception handling
+                                                 //  and Error handling\Democlass.cs:line 24
+
+            }
+            return output;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Exception_handling_and_Error_handling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Democlass demo = new Democlass();
+            int result = demo.grandparent(5);
+
+            Console.WriteLine($"the result for poistin is : {result}");
+            Console.ReadKey();
+        }
+    }
+}
